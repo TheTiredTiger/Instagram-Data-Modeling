0170b950620669ae42f14002ffb8f97ae39747c4
Bárbara Ferreira <166528369+TheTiredTiger@users.noreply.github.com>
2024-07-22 11:31:40 +0000

    Testing

--- src/models.py
+++ src/models.py
@@ -7,26 +7,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user_id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user_id'), nullable=False)
+
+class Comment(Base):
+    __tablename__ = 'comment'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    comment_id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comment_text = Column(String(500), nullable=False)
+    author_id = Column(Integer, ForeignKey('user_id'))
+    post_id = Column(Integer, ForeignKey('post_id'))
+    """ person = relationship(User) """
 
     def to_dict(self):
         return {}
+    
+class Post(Base):
+    post_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user_id'))
+
+class Media(Base):
+    media_id = Column(Integer, primary_key=True)
+    media_type = Column(String(250))
+    url = Column(String, nullable=False)
+    post_id = Column(Integer, ForeignKey('post_id'))
 
 ## Draw from SQLAlchemy base
 try:
